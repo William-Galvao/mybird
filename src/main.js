f8f7a604e7586ff757497ff9019972d7d64f1843
Will <wsg.online@gmail.com>
2022-10-26 20:32:48 -0300

    Done

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import router from "@/routes";
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+
+createApp(App)
+    .use(router)
+    .component('fa', FontAwesomeIcon)
+    .mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue';
+
+
+</script>
+
+<template>
+
+    <HelloWorld message="Vite | Vue (Vue Router) |
+        Tailwind | Font
+        Awesome | Animate.css" />
+
+</template>
